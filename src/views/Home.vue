0a91f03cf7611f9c363170aa6201bea999bf0a71
Ruben L <dummy@email.address>
2019-10-27 12:13:59 +0100

    resolve prettier warnings

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <img alt="Vue logo" src="@/assets/logo.png" />
     <div class="">
       <input type="text" name="" value="" @change="setUrl" @input="setUrl" />
     </div>
